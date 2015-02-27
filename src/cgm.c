542cd55101fb67d86f9fff6bc8cb08a688d3a625
Chris <krystophv@gmail.com>
2015-02-27 14:35:57 -0500

    updating icon constant reference

--- src/cgm.c
+++ src/cgm.c
@@ -2624,7 +2624,7 @@ void handle_minute_tick_cgm(struct tm* tick_time_cgm, TimeUnits units_changed_cg
     if (current_cgm_time == 0) {
         // Init code or error code; set text layer & icon to empty value
         text_layer_set_text(cgmtime_layer, "");
-        create_update_bitmap(&cgmicon_bitmap, cgmicon_layer, TIMEAGO_ICONS[NONE_TIMEAGO_ICON_INDX]);
+        create_update_bitmap(&cgmicon_bitmap, cgmicon_layer, TIMEAGO_ICONS[RCVRNONE_ICON_INDX]);
     } else {
         // set rcvr on icon
         create_update_bitmap(&cgmicon_bitmap, cgmicon_layer, TIMEAGO_ICONS[RCVRON_ICON_INDX]);
@@ -2653,7 +2653,7 @@ void handle_minute_tick_cgm(struct tm* tick_time_cgm, TimeUnits units_changed_cg
             strcat(formatted_cgm_timeago, cgm_label_buffer);
         } else {
             strncpy (formatted_cgm_timeago, "ERR", TIMEAGO_BUFFER_SIZE);
-            create_update_bitmap(&cgmicon_bitmap, cgmicon_layer, TIMEAGO_ICONS[NONE_TIMEAGO_ICON_INDX]);
+            create_update_bitmap(&cgmicon_bitmap, cgmicon_layer, TIMEAGO_ICONS[RCVRNONE_ICON_INDX]);
         }
 
         text_layer_set_text(cgmtime_layer, formatted_cgm_timeago);
